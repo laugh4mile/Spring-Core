c38894ccc03550f2390fbb5cd30b371fe9388158
Hyunsoo <laugh4mile@naver.com>
2022-01-06 17:27:10 +0900

    RateDiscountPolicy 구현 및 RateDiscountPolicyTest 코드 작성

--- src/main/java/spring/core/member/MemoryMemberRepository.java
+++ src/main/java/spring/core/member/MemoryMemberRepository.java
@@ -4,7 +4,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class MemoryMemberRepository implements MemberRepository{
-    private static Map<Long, Member> store = new HashMap<>(); // 사실 동시성 이슈 때문에 ConcurrentHashMap<>()를 써야함
+    private static Map<Long, Member> store = new HashMap<>();
+    // 사실 동시성 이슈 때문에 ConcurrentHashMap<>()를 써야함
 
     @Override
     public void save(Member member) {
